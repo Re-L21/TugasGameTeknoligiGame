4390cb9dbb1e4531826c44892d421faa90d1d533
Re-L21 <gabrielokto21@gmail.com>
2022-05-24 13:51:36 +0700

    raycast added
    not fully functional raycast added (just a debug)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,38 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public GameObject rayObstacle;
     public Camera cam;
+    public SpriteRenderer sp;
 
     Vector2 movement;
     Vector2 mousePos;
     
     void Update()
     {
+        RaycastHit2D obstacleDetected =  Physics2D.Raycast(rayObstacle.transform.position, -Vector2.up, 10f);
+        Debug.DrawRay(rayObstacle.transform.position, -Vector2.up, Color.red);
+
+        if (obstacleDetected.collider != null )
+        {
+            Debug.Log("behind");
+            sp.sortingOrder = 0;
+        }
+        else
+        {
+            Debug.Log("in front");
+            sp.sortingOrder = 2;
+        }
+
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
+
+    // need to fix rotation point for raycast 
 
     private void FixedUpdate()
     {
