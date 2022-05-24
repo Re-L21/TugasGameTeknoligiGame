a28bfe0fe7120fd989097f4279db371cba0ef736
Riel <52339098+Re-L21@users.noreply.github.com>
2022-05-24 19:56:28 +0700

    Revert "raycast added"
    This reverts commit 4390cb9dbb1e4531826c44892d421faa90d1d533.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,38 +7,23 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    public GameObject rayObstacle;
     public Camera cam;
-    public SpriteRenderer sp;
 
     Vector2 movement;
     Vector2 mousePos;
     
     void Update()
     {
-        RaycastHit2D obstacleDetected =  Physics2D.Raycast(rayObstacle.transform.position, -Vector2.up, 10f);
-        Debug.DrawRay(rayObstacle.transform.position, -Vector2.up, Color.red);
+<<<<<<< HEAD
+=======
 
-        if (obstacleDetected.collider != null )
-        {
-            Debug.Log("behind");
-            sp.sortingOrder = 0;
-        }
-        else
-        {
-            Debug.Log("in front");
-            sp.sortingOrder = 2;
-        }
-
-
+>>>>>>> parent of df604bb (Raycast Updated)
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
-
-    // need to fix rotation point for raycast 
 
     private void FixedUpdate()
     {
@@ -47,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+
     }
 
 }
